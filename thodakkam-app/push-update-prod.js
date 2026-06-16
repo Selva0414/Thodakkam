a415edc0441d7067dbaf9909cab03ce3137108f7
selva <selv39629@gmail.com>
2026-06-16 15:22:18 +0530

    Updated project

--- thodakkam-app/push-update-prod.js
+++ thodakkam-app/push-update-prod.js
@@ -1,6 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const { execSync } = require('child_process');
 console.log("Pushing to production branch...");
-execSync('npx eas update --branch production --message "Fix Android Unmount Crash" --non-interactive', { stdio: 'inherit' });
+execSync('npx eas update --branch production --message "Fix Android Unmount Crash" --environment production --non-interactive', { stdio: 'inherit' });
 console.log("Pushing to apk branch...");
-execSync('npx eas update --branch apk --message "Fix Android Unmount Crash" --non-interactive', { stdio: 'inherit' });
+execSync('npx eas update --branch apk --message "Fix Android Unmount Crash" --environment production --non-interactive', { stdio: 'inherit' });
